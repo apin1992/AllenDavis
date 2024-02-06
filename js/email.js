a71140949a215546ac204d2f723c2741cead1f6a
Martin Apresa <japresaperez@gmail.com>
2024-02-05 20:10:28 -0700

    email service

--- js/email.js
+++ js/email.js
@@ -1,17 +1,13 @@
-
-
 function sendMail() {
-    let parms = {
-      name: document.getElementById('name').value,
-      email: document.getElementById('email').value,
-      subject: document.getElementById('subject').value,
-        message: document.getElementById('message').value,
-      number: document.getElementById('number').value
-    };
+  let parms = {
+    name: document.getElementById('name').value,
+    email: document.getElementById('email').value,
+    subject: document.getElementById('subject').value,
+    message: document.getElementById('message').value,
+    number: document.getElementById('number').value,
+  };
 
-    emailjs
-      .send('service_f9hown5', 'template_nqkomyv', parms)
-      .then(alert('Email Sent Successfully! Have a blessed day.'));
-
+  emailjs
+    .send('service_f9hown5', 'template_nqkomyv', parms)
+    .then(alert('Email Sent Successfully! Have a blessed day.'));
 }
-
